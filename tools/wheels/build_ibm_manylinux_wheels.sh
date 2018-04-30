7197387fa6a8dc50dc0c8b3b7023ab86fd256a85
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-04-30 14:09:26 -0400

    fixing wheel script
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- tools/wheels/build_ibm_manylinux_wheels.sh
+++ tools/wheels/build_ibm_manylinux_wheels.sh
@@ -7,8 +7,7 @@ cd xacc-ibm
 for version in cp36-cp36m cp35-cp35m cp34-cp34m cp33-cp33m cp27-cp27m cp27-cp27mu
 do
         export myPython=/opt/python/$version/bin/python
-        $myPython build -t tmp_build --executable="/usr/bin/env python"
         export ver=`case $version in "cp36-cp36m") echo 3.6 ;; "cp35-cp35m") echo 3.5 ;; "cp34-cp34m") echo 3.4 ;; "cp33-cp33m") echo 3.3 ;; "cp27-cp27m") echo 2.7 ;; "cp27-cp27mu") echo 2.7 ;; *) echo "invalid";; esac`
-       $myPython setup.py build -t tmp_build --executable="/usr/bin/env python"
-       $myPython setup.py bdist_wheel --skip-build
+        PYTHONPATH=/xacc/build/lib.linux-x86_64-$ver/xacc $myPython setup.py build -t tmp_build --executable="/usr/bin/env python"
+	PYTHONPATH=/xacc/build/lib.linux-x86_64-$ver/xacc $myPython setup.py bdist_wheel --skip-build
 done
